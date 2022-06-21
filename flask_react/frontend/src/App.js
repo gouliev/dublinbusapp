f6d2e753ee008c7477ea3a4a808f409c896d778d
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2022-06-21 18:35:24 +0100

    Large React Flask Push
    The two scripts which Bo wrote will need to be written in React Code

--- flask_react/frontend/src/App.js
+++ flask_react/frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import useCollapse from 'react-collapsed';
 
 function App() {
   const [getMessage, setGetMessage] = useState({})
@@ -17,16 +18,62 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React + Flask Tutorial</p>
-        <div>{getMessage.status === 200 ? 
-          <h3>{getMessage.data.message}</h3>
-          :
-          <h3>LOADING</h3>}</div>
-      </header>
+      <script src="https://kit.fontawesome.com/ea8bc4d19f.js" crossOrigin="anonymous"></script>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossOrigin="anonymous"></link>
+          <div className="container1">
+        <header className="text-center text-light  my-4">
+            <h1 className="mb-4">Todo List</h1>
+                <input type="text" className="form-control m-auto" name="start" placeholder="start" />
+                <input type="text" className="form-control m-auto" name="end" placeholder="end" />
+            <button type="button" className="btn btn-warning  btn-lg mt-3">-------GO--------</button>
+        </header>
+        <div className="routeList  d-none">
+        </div>
     </div>
+    <div className="container2 my-5 mx-auto">
+
+        <h1 className="text-center my-4 text-light">Weather</h1>
+        <form className="change-location my-4 text-center text-light" id="weatherForm">
+            <label htmlFor="city" className="mb-4">Enter a city for weather information</label>
+            <input type="text" name="city" className="form-control p-4"></input>
+        </form>
+        <div className="card shadow-lg rounded d-none">
+            <img src="https://via.placeholder.com/400x300" className="time card-img-top"></img>
+            <div className="icon bg-light mx-auto text-center">
+                <img src="" alt=""></img>
+            </div>
+            <div className="text-muted text-uppercase text-center details">
+                <h5 className="my-3">city name</h5>
+                <div className="my-3">Weather condition</div>
+                <div className="display-4 my-4">
+                    <span>temp</span>
+                    <span>&deg;</span>
+                </div>
+            </div>
+        </div>
+      </div>
+      <script src="weatherapi.js"></script>
+      <script src="weatherInteractive.js"></script>
+      <script src="searchbox.js"></script>
+  </div>
   );
+}
+
+function Collapsible() {
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+return (
+    <div className="collapsible">
+        <div className="header" {...getToggleProps()}>
+            {isExpanded ? 'Collapse' : 'Expand'}
+        </div>
+        <div {...getCollapseProps()}>
+            <div className="content">
+                Now you can see the hidden content. <br/><br/>
+                Click again to hide...
+            </div>
+        </div>
+    </div>
+    );
 }
 
 export default App;
--- flask_react/frontend/src/index.js
+++ flask_react/frontend/src/index.js
@@ -15,3 +15,139 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/*
+
+const routeList = document.querySelector('.routeList');
+const buttonGo = document.querySelector('button');
+let stopStation = new Array();
+stopStation = [
+    {ssName: "RDS", esName: "UCD", srTime: 20},
+    {ssName: "Smurfit", esName: "OCstreet", srTime: 20}
+]
+
+buttonGo.addEventListener('click', e => {
+    e.preventDefault();
+    if(routeList.classList.contains('d-none')){
+        routeList.classList.remove('d-none');
+    }
+
+    routeList.innerHTML = `            
+    <ul class="list-group choice1 mx-auto text-light mx-3 my-3 ">
+        <p>First Choice</p>
+        <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+            <span>start stop name: ${stopStation[0].ssName}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+            <span>end stop name: ${stopStation[0].esName}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+            <span>scheduled runing time: ${stopStation[0].srTime} mins</span>
+        </li>
+    </ul>
+    <ul class="list-group choice1 mx-auto text-light mx-3 my-3">
+        <p >Second Choice</p>
+        <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+            <span>start stop name: ${stopStation[1].ssName}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+            <span>end stop name: ${stopStation[1].esName}</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+            <span>scheduled runing time: ${stopStation[1].srTime} mins</span>
+        </li>
+    </ul>
+`
+
+});
+
+class Forecast{
+  constructor(){
+      this.key = 'o8A2W5xGSY2kUAieIxpLhJSmHUf33Agv';
+      this.weatherURL = 'http://dataservice.accuweather.com/currentconditions/v1/';
+      this.cityURL = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  }
+  async updateCity(city){
+
+     const cityDets = await this.getCity(city);
+     const weather = await this.getWeather(cityDets.Key);
+     return {cityDets, weather};// same
+
+     }
+  async getCity(city){
+
+     const query = `?apikey=${this.key}&q=${city}`;
+     const response = await fetch(this.cityURL+ query);
+     const data = await response.json();
+     return data[0];
+
+ }
+ async getWeather(id){
+
+     const query = `${id}?apikey=${this.key}`;
+     const response = await fetch(this.weatherURL + query);
+     const data = await response.json();
+     return data[0];
+     
+ }
+
+}
+
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
+
+
+
+const updateUI = (data) => {
+
+    const { cityDets, weather } = data;
+    console.log(data);
+
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;</span>
+        </div>
+    `;
+
+    const iconSrc = `resources/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+    let timeSrc = weather.IsDayTime ? 'resources/day.svg' : 'resources/night.svg';
+
+    time.setAttribute('src', timeSrc);
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+};
+
+
+cityForm.addEventListener('submit', e => {
+    //prevent default actions
+    e.preventDefault();
+    //get city value 
+    const city = cityForm.city.value.trim();
+    console.log(city);
+    cityForm.reset();
+    //update the ui with new city
+    forecast.updateCity(city)
+      .then(data => updateUI(data))
+      .catch(err => console.log(err));
+     //set local storage
+    localStorage.setItem('city', city);
+
+});
+
+// if(localStorage.getItem('city')){
+//     forecast.updateCity(localStorage.getItem('city'))
+//       .then(data => updateUI(data))
+//       .catch(err => console.log(err));
+// }
+
+*/
