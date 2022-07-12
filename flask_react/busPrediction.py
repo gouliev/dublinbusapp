24dc84b195c594f84369d44743957cb2ade5b7f8
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2022-07-12 20:50:23 +0100

    class updated

--- flask_react/busPrediction.py
+++ flask_react/busPrediction.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 #class for producing prediction based results.
 class prediction():
@@ -5,6 +6,38 @@ class prediction():
     def __init__(self, **kwargs):
         self.route = kwargs["route"]
         self.direction = kwargs["direction"]
-        if "day" in kwargs:
-            self.day = kwargs["day"]
-        if  
+        self.day = kwargs["day"]
+        self.hour = kwargs["hour"]
+        self.month = kwargs["month"]
+        self.numberOfStations = kwargs["numberOfStations"]
+        self.data = {'progrnumber':[self.numberOfStations], 'm_2':[0], 'm_3':[0], 'm_4':[0], 'm_5':[0], 'm_6':[0], 'm_7':[0], 'm_8':[0],
+                'm_9':[0], 'm_10':[0], 'h_6':[0], 'h_7':[0],'h_8':[0],'h_9':[0],'h_10':[0],'h_11':[0],'h_12':[0],'h_13':[0],'h_14':[0],
+                'h_15':[0],'h_16':[0],'h_17':[0],'h_18':[0],'h_19':[0],'h_20':[0],'h_21':[0],'h_22':[0],'h_23':[0], 'd_1':[0], 'd_2':[0],
+                'd_3':[0], 'd_4':[0], 'd_5':[0], 'd_6':[0]}
+    
+    #The user will insert the time and date they wish to travel at. Where they are leaving from and going.
+    #Data cleaning will follow the head of the modelTrainerTesting. It has one issue being the inclusion of Index.
+    #Each instance of the class will be cleaned to create a dataframe as displayed https://www.tutorialspoint.com/how-to-create-a-dataframe-in-python#:~:text=To%20create%20a%20dataframe%2C%20we,filled%20in%20the%20dataframe%20table.
+    #This dataframe will then be passed into the pikl file https://practicaldatascience.co.uk/machine-learning/how-to-save-and-load-machine-learning-models-using-pickle
+    #the output will be returned to the user in the form of a JSON data.
+    def _cleanData(self):
+        #here is the data m1-9, h6-23 and d1-6
+        #if user input paramters outside this, make zero.
+        if self.month < 11 and self.month > 1:
+            month = str(self.month)
+            self.data['m_'+month] = 1
+        if self.day < 7 and self.day > 0:
+            day = str(self.day)
+            self.data['d_'+day] = 1
+        if self.hour < 24 and self.hour > 5:
+            hour = str(self.hour)
+            self.data['h_'+hour] = 1
+        return self.data
+    
+    def getPrediction(self):
+        self.data = self._cleanData()
+
+
+month = str(9)
+print('m_'+month)
+        
--- flask_react/routes.py
+++ flask_react/routes.py
@@ -1,10 +1,11 @@
 from flask import send_from_directory
 from flask_react import app
+from busPrediction import prediction
 
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
-@app.route("/busRoute/<route>/<direction>")
-def busPrediction(route, direction):
+@app.route("/busRoute/<route>/<direction>/<day>/<month>/<hour>")
+def busPrediction(route, direction, day, month, hour):
     return "hi"
