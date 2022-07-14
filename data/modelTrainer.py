ebd1d441e06e5586a2685304a1da09b8569e9313
Oscardbf <oscar.deburcafernandez@ucdconnect.ie>
2022-07-14 08:49:11 +0000

    bug fix and new models

--- data/modelTrainer.py
+++ data/modelTrainer.py
@@ -19,7 +19,7 @@ for line,direction in lines:
     df['weekday'] = df.date.dt.weekday
     df['month'] = df.date.dt.month
     df['hour'] = (df.stopActualArr//3600)
-    df = df.drop(columns = ['stopActualArr','rain','temp','pressure','dwelltime'])
+    df.drop(columns = ['stopActualArr','rain','temp','pressure','dwelltime','date','tripid','humidity'], inplace=True)
     df.hour %= 24
     df.month = df.month.astype('category')
     df.hour = df.hour.astype('category')
@@ -28,12 +28,11 @@ for line,direction in lines:
     hourDummies = pd.get_dummies(df.hour,prefix='h', drop_first=True)
     dayDummies = pd.get_dummies(df.weekday, prefix='d', drop_first=True)
     df = pd.concat([df,monthDummies,hourDummies,dayDummies], axis=1)
-    df = df.drop(columns=['month','hour','weekday'])
+    df.drop(columns=['month','hour','weekday'], inplace=True)
     Xfeatures = df.columns[df.columns != 'journeytime']
-    Xfeatures = Xfeatures.drop(['date','tripid','humidity'])
     X = df[Xfeatures]
     y = df.journeytime
-    rfc = RandomForestRegressor(n_estimators=100, oob_score=True, random_state=1)
+    rfc = RandomForestRegressor(n_estimators=5, max_features='auto', random_state=1, max_depth=20, n_jobs=-1)#n jobs -1 uses all processors in parallel
     rfc.fit(X,y)
     with open('models/model{}_{}.pkl'.format(line,direction),'wb') as file:
         pickle.dump(rfc,file, pickle.HIGHEST_PROTOCOL)
