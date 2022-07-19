87b37382c762789c1a6be0d1f97948823ae677fc
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2022-07-19 21:37:45 +0100

    Revert "Merge branch 'main' into develop"
    This reverts commit 57c977eeea8036416b5c59383f99257ff0b551fa, reversing
    changes made to 97159f53dbc5c4462dc180f141d2e0210246c62e.

--- flask_react/frontend/src/components/Weather.js
+++ flask_react/frontend/src/components/Weather.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState, useEffect, useCallback } from "react";
+import { useTheme } from "../hooks/useTheme";
+
 
 export default function Weather() {
     const [key,setKey] = useState('rD2vGykJdSF2ohlaBf7Iycdz8kUFrk79');
@@ -14,6 +16,10 @@ export default function Weather() {
     const [weatherText, setWeatherText] = useState('')
     const [temperature, setTemperature] = useState('')
     const [weatherIcon, setWeatherIcon] = useState('')
+    
+   
+
+
 
 
     const fetchCity = useCallback (async () => {
@@ -21,7 +27,8 @@ export default function Weather() {
       const json = await response.json()
       setCityDets(json[0])
     },[cityURL])
-
+//newyork :349727
+//dublin 207931
     const fetchWeather = useCallback (async () => {
       const response = await fetch(weatherURL + `207931?apikey=${key}`)
       const json = await response.json()
@@ -32,26 +39,26 @@ export default function Weather() {
     useEffect(() => {
       fetchCity()
     },[fetchCity])
-    console.log(cityDets)
+  
 
     useEffect(() => {
       fetchWeather()
-
-
     },[fetchWeather])
+    
     setTimeout(() => {
       setWeatherText(weather.WeatherText)
       setTemperature(weather.Temperature.Metric.Value)
       setWeatherIcon(`img2/icons/${weather.WeatherIcon}.svg`)
       weather.IsDayTime ? setDayOrNight(true) : setDayOrNight(false)
-    },1000);
-    console.log(weather,weatherText,weatherIcon,dayOrNight)
-
+    },2000);
+   
+  
   return (
     <div>
        {dayOrNight && 
           <nav className="navbar navbar-expand-lg bg-light">
             <img src={weatherIcon} alt="" width="70" height="70"/>
+
                 <div className="container-fluid">
                   <div className="collapse navbar-collapse" id="navbarSupportedContent">
                       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -61,14 +68,14 @@ export default function Weather() {
                         <li className="nav-item">
                             <a className="nav-link" href="#">{temperature}℃</a>
                         </li>
-                        
-                        <li className="nav-item">
-                            <a className="nav-link disabled">{cityDets.Localname}</a>
-                        </li>
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Welcome to The Beautiful Dublin</a>
                         </li>
                       </ul>
+                      {/* <form className="d-flex" role="search">
+                        <input className="form-control me-2 cityForm" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                      </form> */}
                   </div>
                 </div>
           </nav>}
@@ -88,11 +95,17 @@ export default function Weather() {
                             <a className="nav-link active" aria-current="page" href="#">Welcome to The Beautiful Dublin</a>
                         </li>
                       </ul>
+                      {/* <form className="d-flex text-white" role="search">
+                        <input className="form-control me-2 cityForm" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                      </form> */}
                   </div>
                 </div>
           </nav>}
     </div>
   )
 }
+
+
 
 
