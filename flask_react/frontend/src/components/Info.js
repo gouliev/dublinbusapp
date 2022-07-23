ba6e3d2902139ffc8feeb65eb302003facd88fa6
Oscardbf <92519436+Oscardbf@users.noreply.github.com>
2022-07-23 17:38:23 +0100

    Reformatted the display of our prediction

--- flask_react/frontend/src/components/Info.js
+++ flask_react/frontend/src/components/Info.js
@@ -19,12 +19,15 @@ export default function Info({
   return (
       <div className="card">
         <ul className="list-group list-group-flush">
-          <li className="list-group-item"><h3>Original station is:{originStation}</h3> </li>
+          <li className="list-group-item"><h3>Original station is: {originStation}</h3> </li>
           <li className="list-group-item"><h3>Destination station is: {destinationStation}</h3> </li>
           <li className="list-group-item"><h3>Total distance is: {distance}</h3> </li>
           <li className="list-group-item"><h3>Transit distance is: {transitDistance}</h3> </li>
-          <li className="list-group-item"> <h2>Total time is: {duration}</h2></li>
-          <li className="list-group-item"> <h2>Transit time is: {transitDuration}</h2></li>
+          <li className="list-group-item"> <h2>Google time is: {duration}</h2></li>
+          <li className="list-group-item"> <h2>Our time is: {Math.trunc(transitDuration/60) > 0? Math.trunc(transitDuration/60)+" hour":null}
+                                                                {Math.trunc(transitDuration/60) > 1? "s ":" "} 
+                                                                {transitDuration%60 > 0? transitDuration%60+" min":null} 
+                                                                {transitDuration%60 > 1? "s":null}</h2></li>
         </ul>
         <div className="card-footer">
           <button onClick={handleCloseAndClearMap} type="button" className="btn btn-danger">Cear Route</button>
