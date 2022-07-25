4053a12f6169ba5af4ad3e3dc229f8ecf1333f8f
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2022-07-25 09:55:17 +0100

    Create the environment settings for the postgres

--- flask_react/__init__.py
+++ flask_react/__init__.py
@@ -3,11 +3,28 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 #This API handler May Not Be Needed
 from flask_react.config import Config
+#This is for the configuration settings
+import configparser
+#SQLAlchemy must be kept in a global environment
+from flask_sqlalchemy import SQLAlchemy
+
+#load in the values from the config file
+config=configparser.ConfigParser()
+config.read('flask_react/config/config.ini')
+key=config.get('oracles','key')
+post=config.get('oracles', 'post')
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/public')
 app.config.from_object(Config)
 CORS(app) #comment this on deployment
 api = Api(app)
+
+#configure the database
+app.config['SECRET_KEY']=key
+#configure the database
+app.config['SQLALCHEMY_DATABASE_URI'] = post
+#initialize db inside of sqlalchemy
+db=SQLAlchemy(app)
 
 
 from flask_react import routes
