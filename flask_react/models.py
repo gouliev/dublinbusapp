68ad4cca1114d07a08421e452082faa11936c32b
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2022-07-26 10:43:04 +0100

    Config Change

--- flask_react/models.py
+++ flask_react/models.py
@@ -1,5 +1,12 @@
 from flask_react import db, app
 import requests
+#This is for the configuration settings 
+import configparser
+
+
+config=configparser.ConfigParser()
+config.read('flask_react/config/config.ini')
+api=config.get('oracles','api')
 
 class Weather(db.Model):
     __tablename__ = "Weather"
@@ -13,15 +20,13 @@ class Weather(db.Model):
     def insertData():
         #delete the current row
         Weather.query.delete()
-        apiURL = 'http://dataservice.accuweather.com/currentconditions/v1/207931?apikey=rD2vGykJdSF2ohlaBf7Iycdz8kUFrk79'
+        apiURL = api
         response = requests.get(apiURL)
         response = response.json()
-        print(response)
         weatherText = response[0]['WeatherText']
         weatherMetric = response[0]['Temperature']['Metric']['Value']
         weatherIcon = response[0]['WeatherIcon']
         weatherTime = response[0]['IsDayTime']
-        print("issue here")
         weather = Weather(weatherText=weatherText, weatherMetric=weatherMetric, weatherIcon=weatherIcon, weatherTime=weatherTime)
         db.session.add(weather)
         db.session.commit()
