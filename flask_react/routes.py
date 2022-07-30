d91c6b66b91d1853aef692a3ccdd4487e643cd64
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2022-07-30 12:16:12 +0100

    OOP

--- flask_react/routes.py
+++ flask_react/routes.py
@@ -1,20 +1,12 @@
 from flask import jsonify
 from flask_react import app
 from flask_react.jsonData import prediction, weatherAPI
-import json
-
-with open('flask_react/models/directions.json', 'r') as f:
-    dictDirs = json.load(f)
-
-def directionConverter(route, direction):
-    if direction in dictDirs.get('{}-2'.format(route), ''):
-        return "2"
-    return "1"
+from flask_react.directions import directionConverterMethod
 
 #this route will be used to access the JSON data.
 @app.route("/busRoute/<iterator>/<route>/<direction>/<stations>/<day>/<month>/<hour>")
 def busPrediction(iterator, route, direction, day, month, hour, stations):
-    direction = directionConverter(route, direction)
+    direction = directionConverterMethod(route=route, direction=direction).directionCheck()
     timePrediction = prediction(day=day, hour=hour, month=month, numberOfStations=stations, route=route, direction=direction, iterator=iterator).jsonPrediction()
     return jsonify(timePrediction)
 
